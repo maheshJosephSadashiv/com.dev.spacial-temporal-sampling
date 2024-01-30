81a94147c4958dafe4cc170406948651b01ef330
maheshJosephSadashiv <mahesh.sadashiv@gmail.com>
2024-01-30 00:40:16 -0800

    game loop

--- ImageDisplay.java
+++ ImageDisplay.java
@@ -20,6 +20,8 @@ public class ImageDisplay {
   	// different dimensions. 
 	int width = 512;
 	int height = 512;
+	private long inputAngle = 0;
+	private double scale = 1;
 	int[][] originalPixelMatrix = new int[height][width];
 	/** Read Image RGB
 	 *  Reads the image of given width and height at the given imgPath into the provided BufferedImage.
@@ -51,7 +53,6 @@ public class ImageDisplay {
 					ind++;
 				}
 			}
-			animate();
 		}
 		catch (Exception e){
 			e.printStackTrace();
@@ -60,15 +61,12 @@ public class ImageDisplay {
 
 	private void animate() throws Exception {
 		imgOne = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-		int inputAngle = 45;
-		int angle = 180 + inputAngle;
-		double scale = 5;
 		for(int y = 0; y < height; y++)
 		{
 			for(int x = 0; x < width; x++)
 			{
 				Coordinates translated = Translate.coordinateSys(new Coordinates(x, y), height, width);
-				double[][] rotated = util.MatrixUtil.rotationAndScale(angle, translated.getxCoordinate(), translated.getyCoordinate(), scale);
+				double[][] rotated = util.MatrixUtil.rotationAndScale(inputAngle, translated.getxCoordinate(), translated.getyCoordinate(), scale);
 				translated = Translate.coordinatePixel(new Coordinates(rotated[0][0], rotated[1][0]), height, width);
 				if (!(translated.getxCoordinate() >= height || translated.getyCoordinate() >= width
 						|| translated.getxCoordinate() < 0 || translated.getyCoordinate() < 0)){
@@ -81,19 +79,20 @@ public class ImageDisplay {
 		}
 	}
 
-	public void showIms(String[] args){
+	public void showIms(String[] args) throws Exception {
 
 		// Read in the specified image
-
 		readImageRGB(width, height, "Lena_512_512.rgb");
 
 		// Use label to display the image
 		frame = new JFrame();
 		GridBagLayout gLayout = new GridBagLayout();
 		frame.getContentPane().setLayout(gLayout);
+		run();
+	}
+	public void run() throws Exception {
 
-		lbIm1 = new JLabel(new ImageIcon(imgOne));
-
+		lbIm1 = new JLabel();
 		GridBagConstraints c = new GridBagConstraints();
 		c.anchor = GridBagConstraints.CENTER;
 		c.weightx = 0.5;
@@ -101,31 +100,32 @@ public class ImageDisplay {
 		c.gridx = 0;
 		c.gridy = 1;
 		frame.getContentPane().add(lbIm1, c);
+		frame.setPreferredSize(new Dimension(width, height));
 		frame.pack();
 		frame.setVisible(true);
+
+		while (true) {
+			// Display current image
+			inputAngle += 45;
+			scale *= 0.95;
+			animate();
+			lbIm1.setIcon(new ImageIcon(imgOne));
+			System.out.println(inputAngle);
+			try {
+				Thread.sleep(300);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 	}
-//	public void run() {
-//		frame.setVisible(true);
-//
-//		while (true) {
-//			// Display current image
-//			imageLabel.setIcon(new ImageIcon(images[currentImageIndex]));
-//
-//			// Update current image index
-//			currentImageIndex = (currentImageIndex + 1) % images.length;
-//
-//			// Delay to control frame rate
-//			try {
-//				Thread.sleep(1000); // Example: 1 second delay
-//			} catch (InterruptedException e) {
-//				e.printStackTrace();
-//			}
-//		}
-//	}
 
 	public static void main(String[] args) {
 		ImageDisplay ren = new ImageDisplay();
-		ren.showIms(args);
+		try {
+			ren.showIms(args);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 }
